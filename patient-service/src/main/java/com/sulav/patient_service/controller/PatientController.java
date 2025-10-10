06b6ead9604880a5f25d01099d99177007ff50c1
Sulav Bajracharya <sulavb22@gmail.com>
2025-10-10 21:50:49 +0545

    Created endpoints for update

--- patient-service/src/main/java/com/sulav/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/sulav/patient_service/controller/PatientController.java
@@ -10,10 +10,13 @@ import com.sulav.patient_service.service.PatientService;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -38,6 +41,14 @@ public class PatientController {
         @Valid @RequestBody PatientRequestDTO patientRequestDTO){
           PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
           return ResponseEntity.ok().body(patientResponseDTO);  
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(
+        @PathVariable UUID id,@RequestBody PatientRequestDTO patientRequestDTO){
+            PatientResponseDTO patientResponseDTO = patientService.updatePatient(
+                id, patientRequestDTO);
+            return ResponseEntity.ok().body(patientResponseDTO);
     }
 
 }
--- patient-service/src/main/java/com/sulav/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/sulav/patient_service/service/PatientService.java
@@ -1,12 +1,15 @@
 package com.sulav.patient_service.service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.sulav.patient_service.dto.PatientRequestDTO;
 import com.sulav.patient_service.dto.PatientResponseDTO;
 import com.sulav.patient_service.exception.EmailAlreadyExistsException;
+import com.sulav.patient_service.exception.PatientNotFoundException;
 import com.sulav.patient_service.mapper.PatientMapper;
 import com.sulav.patient_service.model.Patient;
 import com.sulav.patient_service.repository.PatientRepository;
@@ -40,8 +43,23 @@ public class PatientService {
         return PatientMapper.toDTO(newPatient);
     }
 
+    public PatientResponseDTO updatePatient(UUID id,
+     PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.findById(id).
+        orElseThrow(()-> new PatientNotFoundException("Patient not found with ID: "+id));
+        // if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+        //     throw new EmailAlreadyExistsException("A patient with this email exists already "
+        //     +patientRequestDTO.getEmail());
+        // }
+        
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
 
-
-
+        Patient updatePatient = patientRepository.save(patient);
+        
+        return PatientMapper.toDTO(updatePatient);
+    }
 
 }
