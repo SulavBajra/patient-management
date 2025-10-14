d82cd1acb1053bb2deee076cbb139277abe157b6
Sulav Bajracharya <sulavb22@gmail.com>
2025-10-14 13:07:25 +0545

    Added Deletion of patient

--- patient-service/src/main/java/com/sulav/patient_service/repository/PatientRepository.java
+++ patient-service/src/main/java/com/sulav/patient_service/repository/PatientRepository.java
@@ -10,4 +10,5 @@ import com.sulav.patient_service.model.Patient;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,UUID> {
     boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
 }
