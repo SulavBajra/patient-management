0e7b61eb49cee8e2767a14a2f98ed06286462d50
Sulav Bajracharya <sulavb22@gmail.com>
2025-10-14 11:23:15 +0545

    Added group validators for updating patients

--- patient-service/src/main/java/com/sulav/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/sulav/patient_service/controller/PatientController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sulav.patient_service.dto.PatientRequestDTO;
 import com.sulav.patient_service.dto.PatientResponseDTO;
+import com.sulav.patient_service.dto.validators.CreatePatientValidationGroup;
 import com.sulav.patient_service.service.PatientService;
+import jakarta.validation.groups.Default;
 
 import jakarta.validation.Valid;
 
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,15 +40,16 @@ public class PatientController {
     }
     
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createResponseEntity(
-        @Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDTO> createPatient(
+        @Validated({Default.class, CreatePatientValidationGroup.class}) 
+        @RequestBody PatientRequestDTO patientRequestDTO){
           PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
           return ResponseEntity.ok().body(patientResponseDTO);  
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<PatientResponseDTO> updatePatient(
-        @PathVariable UUID id,@RequestBody PatientRequestDTO patientRequestDTO){
+        @PathVariable UUID id,@Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
             PatientResponseDTO patientResponseDTO = patientService.updatePatient(
                 id, patientRequestDTO);
             return ResponseEntity.ok().body(patientResponseDTO);
---
+++ patient-service/src/main/java/com/sulav/patient_service/dto/validators/CreatePatientValidationGroup.java
@@ -0,0 +1,5 @@
+package com.sulav.patient_service.dto.validators;
+
+public interface CreatePatientValidationGroup {
+    
+}
